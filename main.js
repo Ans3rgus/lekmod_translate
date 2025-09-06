4bf3c2ff1f71d10b44489977352696e07097c1c7
Ans3rgus <kostya.bo4arov2017@yandex.ru>
2025-09-06 16:35:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,26 @@ const nations = [
     "akkad", "aksum", "america", "arabia", "argentina",
 	"armenia","assyria","australia","austria","ayyubids",
 	"aztecs", "babylon", "belgium", "boers", "bolivia",
-	"brazil", "brunei", "bulgaria", "burma", "byzantium"
+	"brazil", "brunei", "bulgaria", "burma", "byzantium",
+	"canada", "carthage", "celts", "chile", "china",
+	"colombia", "cuba", "czechia", "denmark", "egypt",
+	"england", "ethiopia", "finland", "france", "franks",
+	"gaul", "georgia", "germany", "golden_horde", "goths",
+	"greece", "hittites", "hungary", "huns", "inca",
+	"india", "indonesia", "ireland", "iroquois", "israel",
+	"italy", "japan", "jerusalem", "khmer", "kilva",
+	"kongo", "korea", "lithuania", "macedonia", "madagascar",
+	"manchuria", "maori", "maurya", "maya", "mexico",
+	"mongolia", "moors", "morocco", "mughals", "mysore",
+	"nabataea", "netherlands", "new_zealand", "normandy", "norway",
+	"nubia", "oman", "ottomans", "palmyra", "persia",
+	"philippines", "phoenicia", "poland", "polynesia", "portugal",
+	"prussia", "romania", "rome", "russia", "scotland",
+	"shoshone", "siam", "sioux", "songhai", "spain",
+	"sumeria", "sweden", "switzerland", "tibet", "timurids",
+	"tonga", "tunisia", "turkey", "uae", "ukraine",
+	"vatican", "venice", "vietnam", "wales", "yugoslavia",
+	"zimbabwe", "zulu"
 ];
 
 function initNationsDropdown() {
@@ -64,4 +83,5 @@ function initNationsDropdown() {
 
 
 document.addEventListener('DOMContentLoaded', initNationsDropdown);
+
 
